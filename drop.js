eed702c97593ead9dfea555940356219c6ce4f24
LuisYenial <chabadedosqui@gmail.com>
2019-08-27 14:48:53 -0500

    yei inicio

---
+++ drop.js
@@ -0,0 +1,120 @@
+var scaleP = 152
+var scaleC = aleatorio(100, 299)
+var scaleT = scaleP * scaleC
+var cargar = false
+
+var RDM2 = aleatorio(1,4)
+var RDM = aleatorio(1,4)
+
+var PLR = 32000
+var CLR = 200
+
+var PCR = 42000
+var CCR = 500
+
+var PDR = 63000
+var CDR = 300
+
+var PPE = 3000
+var CPE = 1500
+
+var dobledrop = false
+
+var imagenes = []
+imagenes["zulrah"] = "zulrah.png"
+imagenes["LawRune"] = "Lawrune.png"
+imagenes["ChaosRune"] = "Chaosrune.png"
+imagenes["DeathRune"] = "Deathrune.png"
+imagenes["PureEssence"] = "Pureessence.png"
+imagenes["Scales"] = "Scales.png"
+
+tablaxd = document.getElementById("tabla")
+boton = document.getElementById("botoncito")
+boton.addEventListener("click", dropear)
+
+class drops
+{
+    constructor(o,p,q)
+    {
+        this.imagen = new Image()
+        this.objeto = o;
+        this.precio = p;
+        this.cantidad = q;
+        this.imagen.src = imagenes[this.objeto]
+        this.imagen.width = 50
+        this.imagen.height = 50
+    }
+    mostrar()
+    {
+        tabla.appendChild(this.imagen)
+        if(RDM2 == 1 && RDM == 1)
+        {
+            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
+            dobledrop = true
+        }
+        else if(RDM2 == 2 && RDM == 2)
+        {
+            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
+            dobledrop = true
+        }
+        else if(RDM2 == 3 && RDM == 3)
+        {
+            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
+            dobledrop = true
+        }
+        else if(RDM2 == 4 && RDM == 4)
+        {
+            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
+            dobledrop = true
+        }
+        else
+        {
+        tabla.innerHTML += ("x"+ this.cantidad +" "+ this.precio+ " GP ")
+        }
+        tabla.innerHTML += "<hr>"
+    }   
+}
+
+var dropeables = []
+dropeables.push( new drops("Scales",scaleT,scaleC) )
+dropeables.push( new drops("LawRune",PLR,CLR) )
+dropeables.push( new drops("ChaosRune",PCR,CCR) )
+dropeables.push( new drops("DeathRune",PDR,CDR) )
+dropeables.push( new drops("PureEssence",PPE,CPE) )
+
+function dropear() 
+{
+    lastman()
+    dropeables[0].mostrar()
+    if(dobledrop)
+    {
+        dropeables[RDM].mostrar()
+    }
+    else 
+    {   
+    dropeables[RDM].mostrar()
+    dropeables[RDM2].mostrar()
+    }
+    RDM2 = aleatorio(1,4)
+    RDM = aleatorio(1,4)
+    scaleC = aleatorio(100, 299)    
+}
+
+function aleatorio(min,max) 
+{
+    var resultado;
+    resultado = Math.floor(Math.random()*(max - min + 1)) + min    
+    return resultado
+}
+
+function lastman ()
+{
+    tabla.innerHTML +=("<h1>Aqui esta tu Drop:</h1>")
+}
+
+function numberWithCommas(x) 
+{
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+console.log(numberWithCommas(100000000000))
