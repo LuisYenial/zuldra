56ba758d96f7c77319517a5b9b5b7851d8e9bc39
LuisYenial <chabadedosqui@gmail.com>
2019-08-30 14:57:43 -0500

    mejora y limpieza de codigo

--- drop.js
+++ drop.js
@@ -1,16 +1,22 @@
-//items
+//Items
 var scaleP = 152
 var scaleC = aleatorio(100, 299)
 var scaleT = scaleP * scaleC
 var cargar = false
 
+//varForTest
 var total;
 
+//varFroRandomTypes
 var min1;
 var min2;
 var max1;
 var max2;
 
+//kcVars
+var kc = 1
+
+//varsForTypes (discontinued)
 var rarecolor = 15 //parseInt((1/6580) * 100000) //0.015 = 000,015 = 000,015 < 000,000 //15
 var jar = 66//parseInt((1/1500) * 100000) //1500 //0.1 //81
 var raredrop = 195//parseInt((1/512) * 100000) //512 //276
@@ -24,7 +30,7 @@ var raremats = 17000 //parseInt((1/15) * 100000) //15
 var runes = 20724//parseInt((1/12) * 100000) //12
 var mats = 20475//parseInt((1/11) * 100000) //11
 
-/*//pruebas
+/*//test
 console.log(rarecolor)
 total = rarecolor
 console.log(total  + jar)
@@ -51,6 +57,7 @@ console.log(total + mats)
 total +=  mats
 */
 
+//consoleTest
 for(i = 0; i < 1; i++)
 {
     console.log(i)
@@ -58,6 +65,149 @@ for(i = 0; i < 1; i++)
     fstroll2()
 }
 
+
+
+
+//randomDropVar
+var RDM2;
+var RDM1;
+
+//itemVars (maybe discontinued)
+var PLR = 32000
+var CLR = 200
+
+var PCR = 42000
+var CCR = 500
+
+var PDR = 63000
+var CDR = 300
+
+var PPE = 3000
+var CPE = 1500
+
+//doubleDropVar
+var dobledrop = false
+
+
+
+//uglyVars
+var imagenes = []
+//Boss
+imagenes["zulrah"] = "zulrah.png"
+//Runes
+imagenes["LawRune"] = "Lawrune.png"
+imagenes["ChaosRune"] = "Chaosrune.png"
+imagenes["DeathRune"] = "Deathrune.png"
+imagenes["PureEssence"] = "Pureessence.png"
+//Scales
+imagenes["Scales"] = "Scales.png"
+//Mats
+imagenes["Flax"] = "Flax.png"
+imagenes["Mahogany"] = "Mahogany.png"
+imagenes["Yew_logs"] = "Yew_logs.png"
+imagenes["Runite_ore"] = "Runite_ore.png"
+imagenes["Coal"] = "Coal.png"
+imagenes["Snakeskin"] = "Snakeskin.png"
+imagenes["Dragonstone_bolt"] = "Dragonstone_bolt.png"
+
+//editorStart
+tablaxd = document.getElementById("tabla")
+boton = document.getElementById("botoncito")
+boton.addEventListener("click", dropear)
+
+//dropClass
+class drops
+{
+    constructor(o,p,q)
+    {
+        this.imagen = new Image()
+        this.objeto = o;
+        this.precio = p;
+        this.cantidad = q;
+        this.imagen.src = imagenes[this.objeto]
+        this.imagen.width = 50
+        this.imagen.height = 50
+    }
+    //functionForScales
+    mostrar()
+    {
+        tabla.appendChild(this.imagen)
+        tabla.innerHTML += ("x"+ this.cantidad +" "+ this.precio+ " GP ")
+        tabla.innerHTML += "<hr>"
+    }
+    //functionForItems
+    mostrarX2()
+    {
+        tabla.appendChild(this.imagen)
+        if(dobledrop)
+        {
+            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
+            dobledrop = true
+        }
+
+        tabla.innerHTML += "<hr>" 
+    }   
+}
+
+var dropeables = []
+//runas 0-4
+dropeables.push( new drops("Scales",scaleT,scaleC) ) //0
+dropeables.push( new drops("LawRune",PLR,CLR) ) //1
+dropeables.push( new drops("ChaosRune",PCR,CCR) ) //2
+dropeables.push( new drops("DeathRune",PDR,CDR) ) //3
+dropeables.push( new drops("PureEssence",PPE,CPE) ) //4
+//mats 5-11
+dropeables.push( new drops("Flax",2000,1000) ) //5
+dropeables.push( new drops("Mahogany",19050,50) ) //6
+dropeables.push( new drops("Yew_logs",6650,35) ) //7
+dropeables.push( new drops("Runite_ore",22846,2) ) //8
+dropeables.push( new drops("Coal",27000,200) ) //9
+dropeables.push( new drops("Snakeskin",560,35) ) //10
+dropeables.push( new drops("Dragonstone_bolt",2520,12) ) //11
+
+
+
+//la funcion importante °O°
+
+function dropear() 
+{
+    FRASEINIT()
+    //rollOfDice
+    fstroll1()
+    fstroll2()
+    //randomTypeDrop
+    RDM2 = aleatorio(min2,max2)
+    RDM1 = aleatorio(min2,max1)
+    //doubleDropCheck
+    check()
+    //scalesShow
+    dropeables[0].mostrar()
+    //dropShow:
+    //singleItemX2Drop
+    if(dobledrop)
+    {
+        dropeables[RDM1].mostrarX2()
+    }
+    //diferentItems
+    else 
+    {   
+    dropeables[RDM1].mostrar()
+    dropeables[RDM2].mostrar()
+    }
+
+    //scalesQuantityRestart
+    dropeables[0].cantidad = aleatorio(100, 299)
+    //consoletest
+    console.log("Scales: " + dropeables[0].cantidad)
+    console.log(dropeables[1].cantidad)   
+    console.log(dropeables[2].cantidad)
+    //disableDoubleDrop
+    diable()  
+}
+
+//FUNCIONES :3
+
+//1ST ROLL
 function fstroll1()
 {
     var randomEishon;
@@ -118,6 +268,7 @@ function fstroll1()
     }
 }
 
+//2ND ROLL
 function fstroll2()
 {
     var randomEishon;
@@ -177,23 +328,6 @@ function fstroll2()
     }
 }
 
-var RDM2 = aleatorio(min2,max2)
-var RDM1 = aleatorio(min1,max1)
-
-var PLR = 32000
-var CLR = 200
-
-var PCR = 42000
-var CCR = 500
-
-var PDR = 63000
-var CDR = 300
-
-var PPE = 3000
-var CPE = 1500
-
-var dobledrop = false
-
 function diable()
 {
     dobledrop = false
@@ -216,130 +350,42 @@ function check()
     else if(RDM2 == 4 && RDM1 == 4)
     {
         dobledrop = true
-    } 
-}
-
-//vars feas
-
-
-var imagenes = []
-//el boss
-imagenes["zulrah"] = "zulrah.png"
-//runnas
-imagenes["LawRune"] = "Lawrune.png"
-imagenes["ChaosRune"] = "Chaosrune.png"
-imagenes["DeathRune"] = "Deathrune.png"
-imagenes["PureEssence"] = "Pureessence.png"
-//scales
-imagenes["Scales"] = "Scales.png"
-//mats
-imagenes["Flax"] = "Flax.png"
-imagenes["Mahogany"] = "Mahogany.png"
-imagenes["Yew_logs"] = "Yew_logs.png"
-imagenes["Runite_ore"] = "Runite_ore.png"
-imagenes["Coal"] = "Coal.png"
-imagenes["Snakeskin"] = "Snakeskin.png"
-imagenes["Dragonstone_bolt"] = "Dragonstone_bolt.png"
-
-tablaxd = document.getElementById("tabla")
-boton = document.getElementById("botoncito")
-boton.addEventListener("click", dropear)
-
-//creacion de las classes de los items
-class drops
-{
-    constructor(o,p,q)
-    {
-        this.imagen = new Image()
-        this.objeto = o;
-        this.precio = p;
-        this.cantidad = q;
-        this.imagen.src = imagenes[this.objeto]
-        this.imagen.width = 50
-        this.imagen.height = 50
     }
-    mostrar()
+    else if(RDM2 == 4 && RDM1 == 4)
     {
-        tabla.appendChild(this.imagen)
-        tabla.innerHTML += ("x"+ this.cantidad +" "+ this.precio+ " GP ")
-        tabla.innerHTML += "<hr>"
+        dobledrop = true
     }
-    mostrarX2()
+    else if(RDM2 == 5 && RDM1 == 5)
     {
-        tabla.appendChild(this.imagen)
-        if(RDM2 == 1 && RDM1 == 1)
-        {
-            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
-            dobledrop = true
-        }
-        else if(RDM2 == 2 && RDM1 == 2)
-        {
-            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
-            dobledrop = true
-        }
-        else if(RDM2 == 3 && RDM1 == 3)
-        {
-            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
-            dobledrop = true
-        }
-        else if(RDM2 == 4 && RDM1 == 4)
-        {
-            tabla.innerHTML += ("x"+ this.cantidad * 2 +" "+ this.precio * 2+ " GP ")
-            dobledrop = true
-        }
-        tabla.innerHTML += "<hr>" 
-    }   
-}
-
-var dropeables = []
-//runas 0-4
-dropeables.push( new drops("Scales",scaleT,scaleC) ) //0
-dropeables.push( new drops("LawRune",PLR,CLR) ) //1
-dropeables.push( new drops("ChaosRune",PCR,CCR) ) //2
-dropeables.push( new drops("DeathRune",PDR,CDR) ) //3
-dropeables.push( new drops("PureEssence",PPE,CPE) ) //4
-//mats 5-11
-dropeables.push( new drops("Flax",2000,1000) ) //5
-dropeables.push( new drops("Mahogany",19050,50) ) //6
-dropeables.push( new drops("Yew_logs",6650,35) ) //7
-dropeables.push( new drops("Runite_ore",22846,2) ) //8
-dropeables.push( new drops("Coal",27000,200) ) //9
-dropeables.push( new drops("Snakeskin",560,35) ) //10
-dropeables.push( new drops("Dragonstone_bolt",2520,12) ) //11
-
-
-
-
-
-//la funcion importante
-function dropear() 
-{
-    check()
-    lastman()
-    //dropeo y mostreo de scales
-    dropeables[0].mostrar()
-
-    //dropeo y mostreo de drops
-    if(dobledrop)
+        dobledrop = true
+    }
+    else if(RDM2 == 6 && RDM1 == 6)
     {
-        dropeables[RDM1].mostrarX2()
+        dobledrop = true
     }
-    else 
-    {   
-    dropeables[RDM1].mostrar()
-    dropeables[RDM2].mostrar()
+    else if(RDM2 == 7 && RDM1 == 7)
+    {
+        dobledrop = true
     }
+    else if(RDM2 == 8 && RDM1 == 8)
+    {
+        dobledrop = true
+    }
+    else if(RDM2 == 9 && RDM1 == 9)
+    {
+        dobledrop = true
+    }
+    else if(RDM2 == 10 && RDM1 == 10)
+    {
+        dobledrop = true
+    }
+    else if(RDM2 == 11 && RDM1 == 11)
+    {
+        dobledrop = true
+    }
+    tabla.innerHTML += "<hr>" 
+} 
 
-    fstroll1()
-    fstroll2()
-    RDM2 = aleatorio(min2,max2)
-    RDM1 = aleatorio(min2,max1)
-    dropeables[0].cantidad = aleatorio(100, 299) 
-    console.log("Scales: " + dropeables[0].cantidad)
-    console.log(dropeables[1].cantidad)   
-    console.log(dropeables[2].cantidad)
-    diable()  
-}
 
 function aleatorio(min,max) 
 {
@@ -348,14 +394,13 @@ function aleatorio(min,max)
     return resultado
 }
 
-function lastman ()
+function FRASEINIT ()
 {
-    tabla.innerHTML +=("<h1>Aqui esta tu Drop:</h1>")
+    tabla.innerHTML +=("<h1>Kill Count : " + kc + "</h1>")
+    kc = kc+1
 }
 
 function numberWithCommas(x) 
 {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
-//tes
